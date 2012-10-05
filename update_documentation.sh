348a25c78dcada12d10b85ad488c2a06b2bd556c
Reinout van Rees <reinout@vanrees.org>
2012-10-05 16:44:34 +0200

    Added automatic git add/push

--- update_documentation.sh
+++ update_documentation.sh
@@ -1,3 +1,11 @@
 #!/bin/bash
 bin/sphinx
 cp -r doc/build/html/* ../ddsc.github.com
+cd ../ddsc.github.com
+git add -u
+git commit -m "Automatically updated documentation"
+git push
+echo
+echo
+echo "If there are new files, add them by hand."
+git status
